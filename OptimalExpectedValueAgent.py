ad476919acc5549156eb381adb12d2ec410eff21
Benjamin Hadwiger <benjamin.hadwiger@gmx.de>
2023-01-03 17:43:43 +0100

    added doc

--- OptimalExpectedValueAgent.py
+++ OptimalExpectedValueAgent.py
@@ -1,17 +1,12 @@
-import math
-import os.path
-
 from Agent import Agent
 import AgentUtilities
 import Rules
 
-import queue
 import numpy as np
 from itertools import combinations_with_replacement
 from itertools import product
 import math
 import matplotlib.pyplot as plt
-import time
 
 
 class OptimalExpectedValueAgent(Agent):
@@ -20,7 +15,8 @@ class OptimalExpectedValueAgent(Agent):
 
         self.lut_factorial = self.build_lut_factorial()
         self.list_lookup_tables_additional_potential_scores = self.build_potential_additional_score_lookup_tables()
-        self.A, self.weighted_immediate_scores, self.weight_current_score = self.build_system_of_equations_parameters()
+        self.A, self.weighted_immediate_scores, self.weight_current_score = \
+            self.build_system_of_equations_parameters_for_greedy_rethrowing_policy()
         self.expected_additional_scores_when_rethrowing_n_dice = self.solve_linear_system_of_equations()
         self.log_path = log_path
         if self.log_path is not None:
@@ -32,7 +28,7 @@ class OptimalExpectedValueAgent(Agent):
 
     def compute_action(self, obs):
         """
-        Generates an action for a given observation.
+        Computes the action with the highest expected score for a given observation.
 
         :param obs: dict of observations. Example observation:
         dice_values [2 2 3 4 1 1] <class 'numpy.ndarray'>
@@ -65,6 +61,7 @@ class OptimalExpectedValueAgent(Agent):
         list_take_and_fuse = []
         actions_dict = {}
 
+        # TODO: refactor the following code block to be reused for TODO in self.build_system_of_equations_parameters()
         n_multiples = np.sum(face_counts[1:] >= self.min_multiple)
         if n_multiples > 0:
             for val in range(1, self.face_high + 1):
@@ -179,9 +176,47 @@ class OptimalExpectedValueAgent(Agent):
         self.logfile.write(vis_str)
 
     def build_lut_factorial(self):
+        """
+        Creates a lookup table with factorials of the first few natural numbers, i.e.,
+        {1: 1, 2: 2, 3: 6, 4: 24, 5: 120, 6: 720}
+        :return: dict with numbers as keys and factorial of a number as values
+        """
         return {i: np.prod(np.arange(1, i+1)) for i in range(1, self.rules.number_dice+1)}
 
     def build_potential_additional_score_lookup_tables(self, debug=False):
+        """
+        Creates a lookup table with dice combinations for a given number of remaining dice and corresponding scores for
+        a combination. I.e., returns
+        [
+            {
+                (1,): [100, array([ True]), 0.16666666666666666],
+                (2,): [0, array([False]), 0.16666666666666666],
+                (3,): [0, array([False]), 0.16666666666666666],
+                (4,): [0, array([False]), 0.16666666666666666],
+                (5,): [50, array([ True]), 0.16666666666666666],
+                (6,): [0, array([False]), 0.16666666666666666],
+            },
+            {
+                (1, 1): [200, array([ True,  True]), 0.027777777777777776],
+                (1, 2): [100, array([ True, False]), 0.05555555555555555],
+                (1, 3): [100, array([ True, False]), 0.05555555555555555],
+                (1, 4): [100, array([ True, False]), 0.05555555555555555],
+                (1, 5): [150, array([ True,  True]), 0.05555555555555555],
+                (1, 6): [100, array([ True, False]), 0.05555555555555555],
+                (2, 2): [0, array([False, False]), 0.027777777777777776],
+                (2, 3): [0, array([False, False]), 0.05555555555555555],
+                (2, 4): [0, array([False, False]), 0.05555555555555555],
+                (2, 5): [50, array([False,  True]), 0.05555555555555555],
+                (2, 6): [0, array([False, False]), 0.05555555555555555],
+                (3, 3): [0, array([False, False]), 0.027777777777777776]
+                ...
+            },
+            ...
+        ]
+
+        :param debug:
+        :return: a list of dicts with one list element per number of remaining. Each dict maps dice combos to scores
+        """
         list_lookup_tables_additional_potential_scores = []
         for n_remaining in range(1, self.number_dice+1):
             lut_i = {dice_values: list(AgentUtilities.get_potential_score(dice_values, self.rules)) for dice_values in
@@ -206,7 +241,61 @@ class OptimalExpectedValueAgent(Agent):
 
         return list_lookup_tables_additional_potential_scores
 
-    def build_system_of_equations_parameters(self):
+    def build_system_of_equations_parameters_for_greedy_rethrowing_policy(self):
+        """
+        Creates the parameters for a linear system of equations to compute the expected scores.
+
+        When n dice are remaining and rethrown, for n in {1,2,...,6}, for a given n all combinations in the (n-1)th list
+        element in the output of self.build_potential_additional_score_lookup_tables() are possible, yielding the
+        respective scores. E.g., when rethrowing 1 die, the possible outcomes, scores and probabilities are
+
+            (1,): [100, 0.16666],
+            (2,): [0,   0.16666],
+            (3,): [0,   0.16666],
+            (4,): [0,   0.16666],
+            (5,): [50,  0.16666],
+            (6,): [0,   0.16666].
+
+        In case of throwing a 1 or a 5 (prob. 1/3), the round continues and all dice are taken, such that all dice are
+        thrown again; in all other cases (prob 2/3), the current score is lost and it is the next player's turn.
+        So, the expected score can be computed as
+
+            1/6*(current_score + 100) + 1/6*(current_score + 50) + expected_additional_score(n_remaining=6),
+
+        which is equal to
+
+            2/6*current_score + 1/6*100 + 1/6*50 + 2/6*expected_additional_score(n_remaining=6).
+             ^                  \------  -----/        \------------------  ------------------/
+             |                         \/                             \/
+        weight_current_score[0]        |                              |
+                                       |                            f[5] (see text below)
+                          weighted_immediate_scores[0]
+
+        expected_additional_score captures the expected score obtained when continuing the next dice throwi with
+        n_remaining dice.
+
+        This can be extended to all other numbers of remaining dice, and hence is captured in vectors and a matrix of
+        parameters with the number of rows equal to the total number of dice. The weights of current_score are captured
+        in the variable weight_current_score, with weight_current_score[i] representing the weight for
+        n_remaining = i+1. The weighted immediate score are captured in the vector weighted_immediate_scores, with
+        the same mapping of indices. The expected additional scores when rethrowing with n dice are captured in the
+        vector f (again with the same mapping indices), which must be computed and can then be used to determine the
+        action with best expected score.
+
+        For each n, depending on the combination of dice thrown and taken, a particular number of dice are remaining
+        for the next throw. E.g., in the example above, when throwing a 1 or a 5, 6 dice are thrown in the next round,
+        with an expected score of f[6-1] = f[5]. When throwing with two dice and obtaining e.g. (1,4), the 1 die is
+        collected, and one die is remaining for the next throw, which is represented by f[1-1] = f[0]. The likelihood
+        for this combo is obtained from the output of self.build_potential_additional_score_lookup_tables(), and
+        captured in the matrix A. With these parameters, the vector f is then obtained as solution to the linear system
+        of equations (computed subsequently in self.solve_linear_system_of_equations())
+
+            f = Af + weighted_immediate_scores
+
+        The parameters for A, weight_current_score and weighted_immediate_scores are computed in this function.
+
+        :return: matrix A, vector weighted_immediate_scores, vector weight_current_score
+        """
         A = np.zeros([self.number_dice, self.number_dice])
         weight_current_score = np.zeros([self.number_dice])
         weighted_immediate_scores = np.zeros([self.number_dice])
@@ -215,6 +304,7 @@ class OptimalExpectedValueAgent(Agent):
             for k, v in lut_i_remaining.items():
                 if v[0] == 0:
                     continue
+                # TODO: take into account that not all dice that are possible to be taken are necessarily taken
                 weighted_immediate_scores[i] += v[0] * v[2]
                 weight_current_score[i] += v[2]
                 sum_v0 = np.sum(v[1] == 0)
@@ -250,37 +340,37 @@ class OptimalExpectedValueAgent(Agent):
 
 if __name__ == '__main__':
     agent = OptimalExpectedValueAgent()
-    # print(agent.lut_factorial)
+    print(agent.lut_factorial)
+    print('==========================')
+    for lut in agent.list_lookup_tables_additional_potential_scores:
+        for k_, v_ in lut.items():
+            print(f'{k_}: {v_}')
+        print()
+    #
+    # print(f'Coefficient matrix A = \n{agent.A}')
+    # print(f'det(I - A): {np.linalg.det(agent.A - np.eye(6))}')
     # print('==========================')
-    # for lut in agent.list_lookup_tables_additional_potential_scores:
-    #     for k_, v_ in lut.items():
-    #         print(f'{k_}: {v_}')
-    #     print()
-
-    print(f'Coefficient matrix A = \n{agent.A}')
-    print(f'det(I - A): {np.linalg.det(agent.A - np.eye(6))}')
-    print('==========================')
-    # solve (I - A)f = b
-    f = np.linalg.solve(np.eye(6) - agent.A, agent.weighted_immediate_scores)
-    print(f'f solving (I - A)f = b:\n{f}')
-
-    print(agent.weight_current_score)
-    print('==========================')
-    print(agent.weighted_immediate_scores)
-    print('==========================')
-
-    N = 100
-    errors = np.zeros([N,])
-    y = np.zeros([6,])
-    for i in range(N):
-        y = agent.A @ y + agent.weighted_immediate_scores
-        print(y)
-        errors[i] = np.sum((y - f)**2)
-        print(f'Error: {errors[i]}')
-
-    plt.figure()
-    plt.plot(np.log10(errors))
-    plt.grid()
-    plt.xlabel('#Recursions')
-    plt.ylabel('log_10(MSE)')
-    plt.show()
+    # # solve (I - A)f = b
+    # f = np.linalg.solve(np.eye(6) - agent.A, agent.weighted_immediate_scores)
+    # print(f'f solving (I - A)f = b:\n{f}')
+    #
+    # print(agent.weight_current_score)
+    # print('==========================')
+    # print(agent.weighted_immediate_scores)
+    # print('==========================')
+    #
+    # N = 100
+    # errors = np.zeros([N,])
+    # y = np.zeros([6,])
+    # for i in range(N):
+    #     y = agent.A @ y + agent.weighted_immediate_scores
+    #     print(y)
+    #     errors[i] = np.sum((y - f)**2)
+    #     print(f'Error: {errors[i]}')
+    #
+    # plt.figure()
+    # plt.plot(np.log10(errors))
+    # plt.grid()
+    # plt.xlabel('#Recursions')
+    # plt.ylabel('log_10(MSE)')
+    # plt.show()
